dae305c5a6233eb4a2cf46a207330e51ddbbb543
Vladimir <1mt-2@mail.ru>
2022-06-20 18:46:03 +0300

    lesson 21 first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var showPrevBtn = document.getElementById('show-prev');
+var showNextBtn = document.getElementById('show-next');
+
+showPrevBtn.addEventListener('click', onShowPrevBtnClick);
+showNextBtn.addEventListener('click', onShowNextBtnClick);
+
+
+
+function onShowPrevBtnClick () {
+    console.log ("prev is clicked");
+}
+
+
+function onShowNextBtnClick () {
+    console.log ("next is clicked");
+}
+
