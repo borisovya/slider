cbba3e7f9ff539fbd68bce6940f7659d7a39fbc9
Vladimir <1mt-2@mail.ru>
2022-07-12 13:01:48 +0300

    lesson 28

--- car.js
+++ car.js
@@ -1,15 +1,25 @@
-var car1 = {
-    name: 'BMW 3',
-    isTurnOn: false,
-    speed: 0,
-    engine: {
+function Car1(name) {
+    this.name = name;
+    this.isTurnOn = false,
+    this.speed = 0,
+    this.engine = {
         v: 2.01,
         horsePower: 156,
         isTurnOn: false,
-    },
-    start: function() {
-        this.isTurnOn = true;
-        this.speed = 10;
-        this.engine.isTurnOn = true;
-    }
+    };
+    
+};
+
+Car1.prototype.start = function() {
+    this.isTurnOn = true;
+    this.speed = 10;
+    this.engine.isTurnOn = true;
+    console.log(this.name, ' ', this.speed);
 }
+
+var reno = new Car1('reno');
+reno.start();
+var bmw = new Car1('bmw');
+bmw.start();
+bmw.speed = 120;
+console.log('bmw speed = ', bmw.speed);
